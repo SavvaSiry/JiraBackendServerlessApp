8ec84b1eca325e06b6d663b1e9b83a1fefba6486
Savva <savvasiry@gmail.com>
2023-03-19 21:09:58 +0300

    feat(handler): update handler for yandex functions

--- yandex_functions/handler.py
+++ yandex_functions/handler.py
@@ -1,31 +1,71 @@
-import requests
-import jwt
-
-
 def handler(event, context):
-    r = requests.get('https://jsonplaceholder.typicode.com/todos/1')
+    print(event)
     result = use_function(event, context)
     return {
-        'statusCode': result.statusCode,
-        'body': result.body,
+        'statusCode': result["statusCode"],
+        'body': result["body"]
     }
 
 
 def use_function(event, context):
-    path = event["url"]
-    if path == '/path1':
-        return function1(event)
-    elif path == '/path2':
-        return function2(event)
+    path = event["path"]
+    httpMethod = event["httpMethod"]
+
+    if path == '/tasks':
+        if httpMethod == 'POST':
+            return create_task(event)
+        elif httpMethod == 'GET':
+            return get_tasks(event)
+
+    elif path == '/tasks/{id}':
+        id_param = event["params"]["id"]
+        if httpMethod == 'GET':
+            return get_task(event, id_param)
+        elif httpMethod == 'PUT':
+            return update_task(event, id_param)
+        elif httpMethod == 'DELETE':
+            return delete_task(event, id_param)
     else:
-        return function3(event)
+        return error(event)
 
 
-def function1(event):
-    return
+def create_task(event):
+    return {
+        'statusCode': 200,
+        'body': 'POST /tasks'
+    }
 
-def function2(event):
-    return
 
-def function3(event):
-    return
+def get_tasks(event):
+    return {
+        'statusCode': 200,
+        'body': 'GET /tasks'
+    }
+
+
+def get_task(event, id_param):
+    return {
+        'statusCode': 200,
+        'body': 'GET /tasks/{id} ' + id_param
+    }
+
+
+def update_task(event, id_param):
+    return {
+        'statusCode': 200,
+        'body': 'PUT /tasks/{id} ' + id_param
+    }
+
+
+def delete_task(event, id_param):
+    return {
+        'statusCode': 200,
+        'body': 'DELETE /tasks/{id} ' + id_param
+    }
+
+
+def error(event):
+    return {
+        'statusCode': 400,
+        'body': 'Invalid request'
+    }
